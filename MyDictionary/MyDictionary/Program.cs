61432457527f1754112d7144d232f6f86ae29457
Mehmet-Ozan-Ozsoy <62773795+Mehmet-Ozan-Ozsoy@users.noreply.github.com>
2021-01-25 00:34:15 +0300

    GÜN:4 ÖDEV:5(Dictioary,Generics)

---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, double> krediFaiz = new MyDictionary<string, double>();
+            krediFaiz.Add("Konut Kredisi", 1.36);
+            krediFaiz.Add("İhtiyaç Kredisi", 1.64);
+            krediFaiz.Add("Taşıt Kredisi", 1.72);
+
+            Console.Write("[1]Konut Kredisi\n[2]İhtiyaç Kredisi\n[3]Taşıt Kredisi\n\nLütfen kredi seçiminizi yapınız: ");
+            int secim = int.Parse(Console.ReadLine());
+
+            while (secim>0 && secim<4)
+            {
+                Console.WriteLine(krediFaiz.Key[secim - 1] + " -----> %" + krediFaiz.Value[secim - 1]);
+
+                Console.Write("Lütfen kredi seçiminizi yapınız: ");
+                secim = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
